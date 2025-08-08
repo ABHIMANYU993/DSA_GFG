67b774bcb2441ff7ff08cbafced8cfdcd129a8c5
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-08 23:18:58 +0530

    modulo multi and add

---
+++ day_4/modulo_add_multiplication.java
@@ -0,0 +1,10 @@
+class modulo_add_multiplication {
+    public static int sumUnderModulo(int a, int b, int M) {
+        // code here
+        return (a+b)%M;
+    }
+    public int modmul(int a, int b, int M) {
+        // code here
+        return (a*b)%M;
+    }
+}
