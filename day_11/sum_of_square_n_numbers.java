d559df641e4ed981c349283a583347f87e33b97f
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-16 23:26:54 +0530

    sum of square of n numbers

---
+++ day_11/sum_of_square_n_numbers.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class sum_of_square_n_numbers {
+  // Function to calculate the sum of squares of first 'number' natural numbers
+  public int sumOfSquares(int number) {
+    // code here
+    return (number * (number + 1) * (2 * number + 1)) / 6;
+  }
+}
