9b7f00209f6147a6946ce4137af855db0ec5daa4
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-20 23:49:50 +0530

    play with or

---
+++ day_15/play_with_or.java
@@ -0,0 +1,15 @@
+
+// User function Template for Java
+
+class play_with_or {
+
+  // Function for finding maximum and value pair
+  public static int[] game_with_number(int arr[], int n) {
+    // Complete the function
+
+    for (int i = 0; i < arr.length - 1; i++) {
+      arr[i] = arr[i] | arr[i + 1];
+    }
+    return arr;
+  }
+}
