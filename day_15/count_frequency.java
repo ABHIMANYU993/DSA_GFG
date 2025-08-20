053c36e042ddf52842a3d6bb2515c7413d5e5e4e
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-20 22:03:22 +0530

    count frequency of elements

---
+++ day_15/count_frequency.java
@@ -0,0 +1,12 @@
+
+class count_frequency {
+  int countFreq(int[] arr, int target) {
+    // code here
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == target)
+        count++;
+    }
+    return count;
+  }
+}
