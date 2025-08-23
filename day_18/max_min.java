e7fbafff3afffe9ccd8beb64dc81247749d8e709
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-23 21:45:35 +0530

    max and min in array

---
+++ day_18/max_min.java
@@ -0,0 +1,38 @@
+
+// User function Template for Java
+// User function Template for Java
+
+class Pair<K, V> {
+  private final K key;
+  private final V value;
+
+  public Pair(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+}
+
+// Java users need to return result in Pair class
+// For Example -> return new Pair(minimum,maximum)
+
+class max_min {
+  public Pair<Integer, Integer> getMinMax(int[] arr) {
+    // Code Here
+    int max = arr[0], min = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max)
+        max = arr[i];
+      if (arr[i] < min)
+        min = arr[i];
+    }
+    return new Pair(min, max);
+  }
+}
