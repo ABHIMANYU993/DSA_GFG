6c468d2ab091174dd5e3b9fd4925092a649e217e
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-23 12:51:03 +0530

    array duplicates

---
+++ day_18/array_duplicates.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class array_duplicates {
+
+  public int maxnum(int[] arr) {
+    int max = 0;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > arr[max])
+        max = i;
+    }
+    return max;
+  }
+
+  public void frequency(int[] arr, int[] freq) {
+
+    for (int i = 0; i < arr.length; i++) {
+      freq[arr[i]]++;
+    }
+  }
+
+  public ArrayList<Integer> findDuplicates(int[] arr) {
+    // code here
+    ArrayList<Integer> res = new ArrayList<>();
+    int maxVal = arr[maxnum(arr)];
+    int[] freq = new int[maxVal + 1];
+    frequency(arr, freq);
+    for (int i = 0; i < freq.length; i++) {
+      if (freq[i] > 1)
+        res.add(i);
+    }
+    return res;
+  }
+}
