c7463db5461acfb69a9280b442f691aad7df5ef9
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-24 20:07:39 +0530

    set matrix zero

---
+++ day_19/set_matrix_zero.java
@@ -0,0 +1,23 @@
+class set_matrix_zero {
+  public void setMatrixZeroes(int[][] mat) {
+    // code here
+    int[] row = new int[mat.length];
+    int[] col = new int[mat[0].length];
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[i].length; j++) {
+        if (mat[i][j] == 0) {
+          row[i] = 1;
+          col[j] = 1;
+        }
+      }
+    }
+
+    for (int i = 0; i < mat.length; i++) {
+      for (int j = 0; j < mat[i].length; j++) {
+        if (row[i] == 1 || col[j] == 1) {
+          mat[i][j] = 0;
+        }
+      }
+    }
+  }
+}
