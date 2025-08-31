5baa2a328c2ec706e1f7e1172a233ceaf3499027
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-31 19:43:53 +0530

    sum of subarray

---
+++ day_26/sum_of_subarray.java
@@ -0,0 +1,9 @@
+
+class sum_of_subarray {
+    public int subarraySum(int[] arr) {
+        // code here
+        int sum=0;
+        for(int i=0;i<arr.length;i++)sum+=arr[i]*(i+1)*(arr.length-i);
+        return sum;
+    }
+}
