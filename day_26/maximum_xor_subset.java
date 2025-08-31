3a8a59575fb836f57d320fadd6083adf2ab0fc26
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-08-31 22:57:49 +0530

    maximum xor subset

---
+++ day_26/maximum_xor_subset.java
@@ -0,0 +1,37 @@
+/*Complete the function below*/
+
+class maximum_xor_subset {
+  public static int maxSubsetXOR(int arr[], int N) {
+    // add code here.
+
+    int idx = 0, max = 0;
+
+    for (int bit = 31; bit >= 0; bit--) {
+      int maxidx = idx;
+      int maxval = Integer.MIN_VALUE;
+      for (int i = idx; i < arr.length; i++) {
+        if (((arr[i] >> bit) & 1) == 1 && arr[i] > maxval) {
+          maxval = arr[i];
+          maxidx = i;
+        }
+      }
+      if (maxval == Integer.MIN_VALUE)
+        continue;
+
+      int temp = arr[idx];
+      arr[idx] = arr[maxidx];
+      arr[maxidx] = temp;
+
+      for (int i = 0; i < arr.length; i++) {
+        if (i != idx && ((arr[i] >> bit) & 1) == 1)
+          arr[i] ^= arr[idx];
+      }
+      idx++;
+    }
+    for (int i = 0; i < arr.length; i++) {
+      max ^= arr[i];
+    }
+    return max;
+
+  }
+}
